184dbc09ce4b59f92a0c8a5f0d183d6a4820af2c
Winnie Mei <wmei91@gmail.com>
2015-07-21 18:34:08 -0400

    seeded new file

---
+++ seed.sql
@@ -0,0 +1,18 @@
+
+
+
+INSERT INTO CATEGORY (name) VALUES ('Bars');
+INSERT INTO CATEGORY (name) VALUES ('Clubs');
+
+INSERT INTO AUTHOR (author_name, email, location) VALUES ('Winnie Mei', 'wmei91@gmail.com', 'Manhattan');
+INSERT INTO AUTHOR (author_name, email, location) VALUES ('Taylor Swift', 'taylorswift@gmail.com', 'Brooklyn');
+
+
+INSERT INTO ARTICLE (title, time_edited, img_url, article_text, author_id, category_id) VALUES ('Raines Law Room', '7/21/2015', 'http://nightlifeinnewyorkcity.com/wp-content/gallery/raines-law-room-william/raines-law-room-william-inside.jpg', 'Raines Law Rooms strawberry kiss drink is amazing.', 1, 1);
+
+INSERT INTO ARTICLE (title, time_edited, img_url, article_text, author_id, category_id) VALUES ('Angels Share', '7/21/2015', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyG2LmXgijkT5PSvfdhnbmFWkP_CSNC7i57Z7hG9trqSSFpe1DrA', 'Angels Share is amazing.', 2, 2);
+
+
+
+
+
